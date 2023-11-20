2fa83e9029054da46e5b986240387c310d8d8020
Caleb B <calebabaca@gmail.com>
2023-11-20 08:36:43 -0800

    experience start

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import Tilt from "react-tilt";
+import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,7 +8,7 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  // <Tilt className='xs:w-[250px] w-full'>
+  <Tilt className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
@@ -32,7 +32,7 @@ const ServiceCard = ({ index, title, icon }) => (
         </h3>
       </div>
     </motion.div>
-  /* </Tilt> */
+  </Tilt>
 );
 
 const About = () => {
@@ -47,6 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
+       
         I'm a skilled MERN stack software developer with experience inJavaScript,
         and expertise in frameworks like React, Express, Node.js, and Three.js. 
         I'm a quick learner and collaborate closely with clients to create efficient, scalable, 
@@ -62,7 +63,5 @@ const About = () => {
     </>
   );
 };
-
-//this is bullshit, go to BCS and minute mark 74107
 
 export default SectionWrapper(About, "about");
